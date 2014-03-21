d9efc82b0cdcfc273eb4ade9dae5db63293542e1
Bryan Oltman <bryanoltman@gmail.com>
2014-03-21 00:27:45 -0400

    Yay release mode only bugs

--- HatnoteListen/HATViewController.h
+++ HatnoteListen/HATViewController.h
@@ -17,7 +17,7 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *muteButton;
 @property (weak, nonatomic) IBOutlet UIView *userView;
-@property (weak, nonatomic) IBOutlet UILabel *userLabel;
+@property (strong, nonatomic) IBOutlet UILabel *userLabel; // Strong because we juggle this around in its superview and this is less bug-prone than needing to create a strong reference for it every time
 @property (strong, nonatomic) HATAboutViewController *aboutVC;
 
 - (IBAction)muteButtonClicked:(id)sender;
