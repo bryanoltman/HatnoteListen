2e6c5bb88a76374a23529c138d80a63f9512d1a5
Bryan Oltman <bryanoltman@gmail.com>
2014-06-05 16:47:22 -0700

    Add CALayer+Util category

---
+++ HatnoteListen/CALayer+Util.h
@@ -0,0 +1,15 @@
+//
+//  CALayer+Util.h
+//  HatnoteListen
+//
+//  Created by Bryan Oltman on 6/4/14.
+//  Copyright (c) 2014 Bryan Oltman. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CALayer (Util)
+-(void)pauseAnimations;
+-(void)restartAnimations;
+-(void)resumeAnimations;
+@end
