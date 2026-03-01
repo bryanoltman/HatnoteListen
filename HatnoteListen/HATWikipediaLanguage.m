0a801a3bcfcff52c4ecadc61fb7c79a964eda47d
Bryan Oltman <bryanoltman@gmail.com>
2026-02-28 22:31:09 -0500

    update websocket urls

--- HatnoteListen/HATWikipediaLanguage.m
+++ HatnoteListen/HATWikipediaLanguage.m
@@ -9,48 +9,6 @@
 #import "HATWikipediaLanguage.h"
 
 @implementation HATWikipediaLanguage
-+ (NSDictionary*)languageUrlMap {
-  static NSDictionary* langs = nil;
-  if (!langs) {
-    langs = @{
-      @"en" : @"ws://wikimon.hatnote.com:9000",
-      @"de" : @"ws://wikimon.hatnote.com:9010",
-      @"ru" : @"ws://wikimon.hatnote.com:9020",
-      @"uk" : @"ws://wikimon.hatnote.com:9310",
-      @"ja" : @"ws://wikimon.hatnote.com:9030",
-      @"es" : @"ws://wikimon.hatnote.com:9040",
-      @"fr" : @"ws://wikimon.hatnote.com:9050",
-      @"nl" : @"ws://wikimon.hatnote.com:9060",
-      @"it" : @"ws://wikimon.hatnote.com:9070",
-      @"sv" : @"ws://wikimon.hatnote.com:9080",
-      @"ar" : @"ws://wikimon.hatnote.com:9090",
-      @"fa" : @"ws://wikimon.hatnote.com:9210",
-      @"he" : @"ws://wikimon.hatnote.com:9230",
-      @"id" : @"ws://wikimon.hatnote.com:9100",
-      @"zh" : @"ws://wikimon.hatnote.com:9240",
-      @"as" : @"ws://wikimon.hatnote.com:9150",
-      @"hi" : @"ws://wikimon.hatnote.com:9140",
-      @"bn" : @"ws://wikimon.hatnote.com:9160",
-      @"pa" : @"ws://wikimon.hatnote.com:9120",
-      @"te" : @"ws://wikimon.hatnote.com:9160",
-      @"ta" : @"ws://wikimon.hatnote.com:9110",
-      @"ml" : @"ws://wikimon.hatnote.com:9250",
-      @"mr" : @"ws://wikimon.hatnote.com:9130",
-      @"kn" : @"ws://wikimon.hatnote.com:9170",
-      @"or" : @"ws://wikimon.hatnote.com:9180",
-      @"sa" : @"ws://wikimon.hatnote.com:9190",
-      @"gu" : @"ws://wikimon.hatnote.com:9200",
-      @"pl" : @"ws://wikimon.hatnote.com:9260",
-      @"mk" : @"ws://wikimon.hatnote.com:9270",
-      @"be" : @"ws://wikimon.hatnote.com:9280",
-      @"bg" : @"ws://wikimon.hatnote.com:9300",
-      @"sr" : @"ws://wikimon.hatnote.com:9290",
-      @"wikidata" : @"ws://wikimon.hatnote.com:9220"
-    };
-  }
-
-  return langs;
-}
 
 + (NSDictionary*)languageNamesToCodes {
   NSDictionary* langs = nil;
@@ -95,11 +53,8 @@
 }
 
 + (NSURL*)websocketURLForLanguageCode:(NSString*)languageCode {
-  NSString* urlString = [self languageUrlMap][languageCode];
-  if (!urlString) {
-    return nil;
-  }
-  return [NSURL URLWithString:urlString];
+  static NSString* urlBase = @"wss://wikimon.hatnote.com/v2/";
+  return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", urlBase, languageCode]];
 }
 
 - (id)initWithCoder:(NSCoder*)decoder {
